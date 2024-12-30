f23fb1ebd319d0d7b689d5599ea80209b9a25b02
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2024-12-31 00:01:41 +0700

    fix: login and register

--- lad/src/main/Loginwindow/loginscript.js
+++ lad/src/main/Loginwindow/loginscript.js
@@ -18,3 +18,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded');
+    const loginButton = document.getElementById('login-button');
+    console.log('Login button element:', loginButton);
+    
+    loginButton.addEventListener('click', () => {
+        console.log('Login button clicked');
+        const username = document.getElementById('username');
+        const password = document.getElementById('password');
+        console.log('Username element:', username);
+        console.log('Password element:', password);
+
+        if (username.value === 'ITCSIU23007' && password.value ==='Xaz73159') {
+            console.log('Credentials correct, attempting redirect');
+            window.location.href = '../AdminPage/Admin_UI.html';
+        } else {
+            alert('Invalid username or password.');
+        }
+    });
+});
+
+document.getElementById('register').addEventListener('click', () => {
+    console.log('Redirecting to register.html');
+    window.location.href = '/lad/src/main/Register/register.html';
+});

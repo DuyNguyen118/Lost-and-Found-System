3238774ab70566bf66b406bbcec5d1adcbf0bde3
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2025-01-04 12:07:12 +0700

    fix: delete report date and decorate the filter box

--- lad/src/main/Frontend/MainPage/script.js
+++ lad/src/main/Frontend/MainPage/script.js
@@ -56,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
                         <td>${item.location}</td>
                         <td>${item.room ? item.room.roomNumber : ''}</td>
                         <td>${item.status}</td>
-                        <td>${item.reportDate}</td>
                         <td>${item.contactInfo}</td>
                     `;
 
